bc08460dff9f4c33c5b2fc65a96663a23fe1327e
Daniel Godin <DanielEGodin@gmail.com>
2024-10-21 14:09:12 -0700

    Firebase Config Fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,8 @@
 // Import the functions you need from the SDKs you need
+import { firebaseConfig } from "../firebaseConfig";
 import { initializeApp } from "firebase/app";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDR6EZsKRAumg3hR-hfuZeBI5bmjxzVbC0",
-  authDomain: "mileage-a145a.firebaseapp.com",
-  projectId: "mileage-a145a",
-  storageBucket: "mileage-a145a.appspot.com",
-  messagingSenderId: "687709479761",
-  appId: "1:687709479761:web:f57e176694b0ca50e02970"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
